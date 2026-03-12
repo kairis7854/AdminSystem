06564d0e231e27f2c4e2fb870354a93440b1d2c9
kairis7854 <kairis7854@gmail.com>
2026-03-12 18:00:33 +0800

    style:login UI

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import "./login.scss"
 
 export default function Login() {
+
+
+
+
+
+
+
+
+
+
   return (
-    <div>Login</div>
+    <div className="login">
+      <form className="loginCard">
+        <h2 className="loginCard__title">後臺管理系統</h2>
+
+        <label className="loginCard__user">
+          <span >帳號</span>
+          <input className="loginCard__userInput" type="text" name="username" required  autoComplete="off"/>
+        </label>
+
+        <label className="loginCard__password">
+          <span >密碼</span>
+          <input className='loginCard__passwordInput' type="password" name="passwordInput" required  autoComplete="off" />
+        </label>
+
+        <button type="submit" className="loginCard__button">登入</button>
+      </form>
+    </div>
   )
 }

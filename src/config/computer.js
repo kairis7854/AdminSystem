1c696bb874085fd20ec8d0991789765fa921d1bb
kairis7854 <kairis7854@gmail.com>
2026-03-26 16:08:53 +0800

    test: 添加測試資料

--- src/config/computer.js
+++ src/config/computer.js
@@ -3,6 +3,12 @@ import iMac from '../../src/assets/computerImge/iMac.jpg'
 import MacBookNeo from '../../src/assets/computerImge/MacBookNeo.jpg'
 import macbookAir13 from '../../src/assets/computerImge/macbookAir13.png'
 import MacBookPro14 from '../../src/assets/computerImge/MacBookPro14.png'
+import MSI_Katana_17_HX from '../../src/assets/computerImge/MSI_Katana_17_HX.png'
+import MSI_Prestige_14_AI from '../../src/assets/computerImge/MSI_Prestige_14_AI+.png'
+import ASUS_ExpertBook_CX54 from '../../src/assets/computerImge/ASUS_ExpertBook_CX54.png'
+import ROG_Zephyrus_G14_2006 from '../../src/assets/computerImge/ROG_Zephyrus_G14_2006.png'
+import galaxy_book5 from '../../src/assets/computerImge/galaxy_book5.png'
+import galaxy_book6_ultra from '../../src/assets/computerImge/galaxy_book6_ultra.png'
 
 export const computerProducts = [
   {
@@ -21,7 +27,7 @@ export const computerProducts = [
   },
   {
     id: 'mac-pro',
-    name: "MacBookPro14",
+    name: "MacBook Pro 14",
     brand: "Apple",
     price: 54900,
     image: MacBookPro14
@@ -33,62 +39,50 @@ export const computerProducts = [
     price: 42900,
     image: iMac
   },
-  {
-    id: 'mac-neo1',
-    name: "MacBook Neo",
-    brand: "Apple",
-    price: 19900,
-    image: MacBookNeo
-  },
-  {
-    id: 'mac-air1',
-    name: "MacBook Air",
-    brand: "Apple",
-    price: 35900,
-    image: macbookAir13
-  },
-  {
-    id: 'mac-pro1',
-    name: "MacBookPro14",
-    brand: "Apple",
+   {
+    id: 'MSI_Katana_17_HX',
+    name: "Katana 17 HX",
+    brand: "MSI",
     price: 54900,
-    image: MacBookPro14
+    image: MSI_Katana_17_HX
   },
-  {
-    id: 'imac1',
-    name: "iMac",
-    brand: "Apple",
-    price: 42900,
-    image: iMac
+   {
+    id: 'MSI_Prestige_14_AI',
+    name: "Prestige 14 AI",
+    brand: "MSI",
+    price: 57900,
+    image: MSI_Prestige_14_AI
   },
-  {
-    id: 'mac-neo2',
-    name: "MacBook Neo",
-    brand: "Apple",
-    price: 19900,
-    image: MacBookNeo
+   {
+    id: 'ASUS-ExpertBook-CX54',
+    name: "ASUS ExpertBook CX54",
+    brand: "ASUS",
+    price: 31900,
+    image: ASUS_ExpertBook_CX54
   },
-  {
-    id: 'mac-air2',
-    name: "MacBook Air",
-    brand: "Apple",
-    price: 35900,
-    image: macbookAir13
+   {
+    id: 'ROG_Zephyrus_G14(2026)',
+    name: "ROG Zephyrus G14(2026)",
+    brand: "ASUS",
+    price: 69999,
+    image: ROG_Zephyrus_G14_2006
   },
-  {
-    id: 'mac-pro2',
-    name: "MacBookPro14",
-    brand: "Apple",
-    price: 54900,
-    image: MacBookPro14
+   {
+    id: 'galaxy_book5',
+    name: "Galaxy book5",
+    brand: "Samsung ",
+    price: 49600,
+    image: galaxy_book5
   },
-  {
-    id: '2',
-    name: "iMac",
-    brand: "Apple",
-    price: 42900,
-    image: iMac
+   {
+    id: 'galaxy_book6_ultra',
+    name: "Galaxy galaxy book6 ultra",
+    brand: "Samsung ",
+    price: 49600,
+    image: galaxy_book6_ultra
   },
+   
+ 
 ];
 
 export default computerProducts
